e74dba40b1e21c07e43eb7f914dcb61b28751e23
lushun <lushun1@lenovo.com>
2020-07-19 21:03:49 +0800

    初始化 feign

---
+++ hello-spring-cloud-web-admin-feign/src/main/java/com/funtl/hello/spring/cloud/web/admin/feign/WebAdminFeignApplication.java
@@ -0,0 +1,21 @@
+package com.funtl.hello.spring.cloud.web.admin.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @Author ls
+ * @Date 2020/07/19 20:54:50
+ * @Description
+ * @Version 1.0
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class WebAdminFeignApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebAdminFeignApplication.class,args);
+    }
+}
