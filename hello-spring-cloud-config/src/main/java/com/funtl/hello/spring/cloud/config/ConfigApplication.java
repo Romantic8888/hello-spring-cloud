8dc248b76fed295175de38b200075fd96231d417
lushun <lushun1@lenovo.com>
2020-08-07 16:57:02 +0800

    新增分布式配置中心

---
+++ hello-spring-cloud-config/src/main/java/com/funtl/hello/spring/cloud/config/ConfigApplication.java
@@ -0,0 +1,23 @@
+package com.funtl.hello.spring.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import javax.security.auth.login.Configuration;
+
+/**
+ * @Author ls
+ * @Date 2020/08/07 16:18:00
+ * @Description
+ * @Version 1.0
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Configuration.class,args);
+    }
+}
