bfbe7a79ab45c93c6dce6cab36ef49a2288357f8
lushun <lushun1@lenovo.com>
2020-09-02 11:33:14 +0800

    新增链路追踪 配置

---
+++ hello-spring-cloud-zipkin/src/main/java/com/funtl/hello/spring/cloud/zipkin/ZipKinApplication.java
@@ -0,0 +1,21 @@
+package com.funtl.hello.spring.cloud.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.internal.EnableZipkinServer;
+
+/**
+ * @Author ls
+ * @Date 2020/09/02 10:59:49
+ * @Description
+ * @Version 1.0
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class ZipKinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipKinApplication.class,args);
+    }
+}
