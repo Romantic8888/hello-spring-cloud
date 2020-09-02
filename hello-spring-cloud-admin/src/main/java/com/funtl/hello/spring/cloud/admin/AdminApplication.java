90cf8ee57413a5e9b30aed2e621c3610e5c5581b
lushun <lushun1@lenovo.com>
2020-09-02 18:38:54 +0800

    新增 spring boot admin

---
+++ hello-spring-cloud-admin/src/main/java/com/funtl/hello/spring/cloud/admin/AdminApplication.java
@@ -0,0 +1,21 @@
+package com.funtl.hello.spring.cloud.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author ls
+ * @Date 2020/09/02 17:26:37
+ * @Description
+ * @Version 1.0
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+@EnableAdminServer
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class,args);
+    }
+}
